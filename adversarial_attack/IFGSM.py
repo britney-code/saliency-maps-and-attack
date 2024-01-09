20151b2a1faa1ebc4a8da08125d499a3f109a514
wanqiang512 <1370192111@qq.com>
2024-01-09 17:52:31 +0800

    Update IFGSM.py

--- adversarial_attack/IFGSM.py
+++ adversarial_attack/IFGSM.py
@@ -1,41 +1,36 @@
 import torch
 import torch.nn as nn
-from torchvision.transforms import Normalize
+from Normalize import Normalize
+import torchvision
 
-
-class IFGSM:
+class ifgsm:
     def __init__(self,
                  steps,
                  eps,
-                 device=torch.device("cuda:0" if torch.cuda.is_available() else "cpu")):
+                 device=torch.device("cuda:0" if torch.cuda.is_available() else "cpu"),
+                 mean = [0.485, 0.456, 0.406],
+                 std = [0.229, 0.224, 0.225]
+    ):
         self.steps = steps
         self.device = device
         self.eps = eps
+        self.trans = Normalize(mean, std)
+        self.alpha = self.eps / self.steps 
 
-    def TNormalize(self, x, IsRe, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]):
-        if not IsRe:
-            x = Normalize(mean=mean, std=std)(x)
-        elif IsRe:
-            # tensor.shape:(3,w.h)
-            for idx, i in enumerate(std):
-                x[:, idx, :, :] *= i
-            for index, j in enumerate(mean):
-                x[:, index, :, :] += j
-        return x
+  
 
     def attack(self, model, images, labels):
         images = images.clone().detach().to(self.device)
         labels = labels.clone().detach().to(self.device)
         adv = images.clone().detach()
-        alpha = self.eps / self.steps
 
         for i in range(self.steps):
             adv.requires_grad = True
-            logits = model(self.TNormalize(adv))
+            logits = model(self.trans(adv))
             ce_loss = nn.CrossEntropyLoss()  # loss = F.nll_loss(logits, labels)
             loss = ce_loss(logits, labels)
             loss.backward()
-            adv = adv + alpha * torch.sign(adv.grad)
+            adv = adv + self.alpha * torch.sign(adv.grad)
             model.zero_grad()
             diff = adv - images
             delta = torch.clamp(diff, -self.eps, self.eps)
