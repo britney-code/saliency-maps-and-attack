b087781b50d34a627c7f89271573fa0a53d6b4f9
britney-code <1370192111@qq.com>
2024-03-29 17:58:41 +0800

    Update attack_methods.py

--- attack/attack_methods.py
+++ attack/attack_methods.py
@@ -5,7 +5,7 @@ import numpy as np
 
 
 """Input diversity: https://arxiv.org/abs/1803.06978"""
-def DI(x, resize_rate=1.1, diversity_prob=0.7):
+def DI(x, resize_rate=1.15, diversity_prob=0.7):
     assert resize_rate >= 1.0
     assert diversity_prob >= 0.0 and diversity_prob <= 1.0
     img_size = x.shape[-1]
@@ -21,6 +21,27 @@ def DI(x, resize_rate=1.1, diversity_prob=0.7):
     padded = F.pad(rescaled, [pad_left.item(), pad_right.item(), pad_top.item(), pad_bottom.item()], value=0)
     ret = padded if torch.rand(1) < diversity_prob else x
     return ret
+
+
+# 另一个版本的DI，主要是把数据缩放到指定的大小
+def input_diversity(x, diversity_prob = 0.7, resize_rate = 1.1):
+        # do not transform the input image
+        if torch.rand(1) > diversity_prob:   return x
+        img_size = x.shape[-1]
+        img_resize = int(img_size * resize_rate)
+        # resize the input image to random size
+        rnd = torch.randint(low=min(img_size, img_resize), high=max(img_size, img_resize), size=(1,), dtype=torch.int32)
+        rescaled = F.interpolate(x, size=[rnd, rnd], mode='bilinear', align_corners=False)
+        # randomly add padding
+        h_rem = img_resize - rnd
+        w_rem = img_resize - rnd
+        pad_top = torch.randint(low=0, high=h_rem.item(), size=(1,), dtype=torch.int32)
+        pad_bottom = h_rem - pad_top
+        pad_left = torch.randint(low=0, high=w_rem.item(), size=(1,), dtype=torch.int32)
+        pad_right = w_rem - pad_left
+        padded = F.pad(rescaled, [pad_left.item(), pad_right.item(), pad_top.item(), pad_bottom.item()], value=0)
+        # resize the image back to img_size
+        return F.interpolate(padded, size=[img_size, img_size], mode='bilinear', align_corners=False)
 
 # TI
 def gkern(kernlen=15, nsig=3):
